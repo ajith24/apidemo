c930e2cfccb5b6c854796a0a643b0bf379ebe0e9
Ajith <ajith.joyal@ekino.com>
2019-04-15 19:37:08 +0400

    sql

--- ajithApi/protected/data/schema.mysql.sql
+++ ajithApi/protected/data/schema.mysql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 15, 2019 at 11:13 AM
+-- Generation Time: Apr 15, 2019 at 03:36 PM
 -- Server version: 5.7.24
 -- PHP Version: 5.6.40
 
@@ -38,15 +38,62 @@ CREATE TABLE IF NOT EXISTS `product` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`id`, `name`, `cost`, `vat_class`, `barcode`, `created_at`, `updated_at`) VALUES
-(1, 'mob', '300', 6, '123123', '2019-04-15 14:16:16', '2019-04-15 14:16:16'),
-(2, 'iphone', '2500', 20, '234234', '2019-04-15 14:56:21', '2019-04-15 14:56:21');
+(1, 'Mobile', '867', 6, '123123', '2019-04-15 14:16:16', '2019-04-15 14:16:16'),
+(2, 'iPhone', '2500', 20, '234234', '2019-04-15 14:56:21', '2019-04-15 14:56:21'),
+(3, 'Laptop', '2400', 6, '2313113', '2019-04-15 19:34:39', '2019-04-15 19:34:39'),
+(4, 'PC', '2300', 6, '321432', '2019-04-15 19:35:51', '2019-04-15 19:35:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt`
+--
+
+DROP TABLE IF EXISTS `receipt`;
+CREATE TABLE IF NOT EXISTS `receipt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rid` int(11) DEFAULT NULL,
+  `product_id` int(11) NOT NULL DEFAULT '0',
+  `product_total` decimal(10,0) NOT NULL DEFAULT '0',
+  `product_discount` decimal(10,0) NOT NULL DEFAULT '0',
+  `vat` int(11) NOT NULL DEFAULT '0',
+  `total_price` decimal(10,0) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `salt` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `profile` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `salt`, `email`, `profile`) VALUES
+(1, 'demo', '2e5c7db760a33498023813489cfadc0b', '28b206548469ce62182048fd9cf91760', 'webmaster@example.com', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
